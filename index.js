8c4488c39ab93c8097b8703b250c674921a9ca9f
snstrauss <zahavi.guy.85@gmail.com>
2021-03-06 11:28:26 +0200

    initial commit - getting ip and saving it to clipboard

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const { networkInterfaces } = require("os");
+const clipboardy = require("clipboardy");
+
+const isIPv4 = (net) => net.family === "IPv4";
+const isExternal = (net) => !net.internal;
+
+const { address } = Object.values(networkInterfaces())
+    .filter((networkInterface) => networkInterface.some(isExternal))
+    .filter((nets) => nets.some(isIPv4))[0]
+    .filter(isIPv4)[0];
+
+clipboardy.writeSync(address);
+
+console.log();
+console.log(`Address ${address} copied to clipboard`);
+console.log();
+
+// const thing = Object.values(networkInterfaces())
+//     .filter((networkInterface) => networkInterface.some(isExternal))
+//     .filter((nets) => nets.some(isIPv4))[0]
+//     .filter(isIPv4)[0].address;
